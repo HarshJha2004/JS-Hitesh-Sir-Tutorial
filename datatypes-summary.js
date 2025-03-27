f9cbfe0e7c4a33f779fc583bcc40dceb7b1b747a
Harsh Jha <harshwork5546@gmail.com>
2025-03-27 13:12:38 +0530

    Few companies questions

---
+++ datatypes-summary.js
@@ -0,0 +1,9 @@
+// There are two types of datatypes.
+    // 1.primitive => 7 types.
+        // String, symbol, Number, Boolean, Undefined, Null, BigInt
+        // kisi bhi value ko unique banane ke liye symbol use hota hai.
+
+
+    // 2.Non-primitive / Reference-Type.
+        //Memory me iska reference direct allocate kiya ja sakta hai.
+        // -Array, Objects, Functions.
