b580b1dd092f23cf1c8c9299a5642479a5569bea
Harsh Jha <harshwork5546@gmail.com>
2025-03-28 10:21:14 +0530

    Added some String concepts but the String Method reading are Pending

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const repoName = "Harsh Jha"
+const repoCount = 10
+
+//Concatenation infromation.
+
+    // console.log(repoName + repoCount + "total repo count"); => Bad practice.
+
+    // console.log(`Hello my name is ${repoName} and my Github repo count is ${repoCount}`); => good practice
+
+// other ways to declare a string.
+
+    const gameName = new String('FreeFire')
+
+//saare String Methods bhi padhne hai documentation se.(Remaining)
