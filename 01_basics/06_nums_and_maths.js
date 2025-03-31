4f9c0b2bbdc77b0835ee14b9ea49cb9cccbd64cd
Harsh Jha <harshwork5546@gmail.com>
2025-03-31 09:14:07 +0530

    Some Maths related topics explained

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400
+console.log(score);
+
+// explicity define type.
+
+const bankBalance = new Number(250)
+console.log(bankBalance);
+
+// use methods.
+    // console.log(bankBalance.toString().length);
+    // console.log(bankBalance.toFixed(1));
+
+//+++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++
+
+    // console.log(Math);
+    // console.log(Math.abs(-4));
+    // console.log(Math.round(4.3));
+    // console.log(Math.ceil(4.2));
+    // console.log(Math.floor(4.6));
+
+
+//The (Math) library is mostly used in random values
+    console.log(Math.random())
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JS-Hitesh-Sir-Tutorial
 JS tutorials by Hitesh sir youtube channel.
+
+Read(Gothrough) String Methods.
+Read(Gothrough) Math Methods.
