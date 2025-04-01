5c79a86594c69d9ed1cdd814110e5c3eef318174
Harsh Jha <harshwork5546@gmail.com>
2025-04-01 07:37:22 +0530

    Array-part-1 Completed

---
+++ 02_basics/01_array.js
@@ -0,0 +1,24 @@
+const Arr1 = [1, 2, 3, 4, 5]
+// For view the indexing.
+    // console.log(Arr1[1]);
+
+// Array Methods.
+    // Arr1.push(6)
+    // Arr1.push(7)
+    // console.log(Arr1);
+
+// In POP operation the last value is deleted.
+    // console.log("After using POP operation.")
+    // Arr1.pop()
+    // console.log(Arr1);
+
+// Shift and unshift method PUSH and POP ki tarah hi kaam karta hai.Ye mostly TODO projects banane ma kaam atta hai.ye elements ko sabse aage me rakh deta hai.
+    // console.log(Arr1);
+    // Arr1.unshift(9)
+    // Arr1.shift()
+    // console.log(Arr1);
+    
+// slice and splice => Read from documentation.
+
+   
+    
